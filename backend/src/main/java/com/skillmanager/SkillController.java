6fab10a9abfc728f6d6f16efe5ac86f8992a765b
ultradr3mer <dulz@hotmail.de>
2026-06-25 18:23:20 +0200

    rename

--- backend/src/main/java/com/skillmanager/SkillController.java
+++ backend/src/main/java/com/skillmanager/SkillController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.skillmanager;
 
 import java.io.IOException;
 
--- backend/src/main/java/com/skillmanager/SkillManagerApplication.java
+++ backend/src/main/java/com/skillmanager/SkillManagerApplication.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package com.skillmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SkillManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SkillManagerApplication.class, args);
     }
 }
